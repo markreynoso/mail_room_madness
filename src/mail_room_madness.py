5e1ec9a48aa385fb6401d54fcaf9251e19e642c4
Mark Reynoso <mreynoso@spu.edu>
2017-10-20 16:40:20 -0700

    created donor dictionary and function to populate it

---
+++ src/mail_room_madness.py
@@ -0,0 +1,57 @@
+"""This file contains psuedocode for writing thank you notes to non-profit donors"""
+
+donor_data = [{'name': 'Phil Collins', 'donations': [25, 45, 76, 100]}, {'name': 'Sven Sunguaard', 'donations': [50, 1000, 76, 1400]}]
+
+# def main():
+#     call all the things
+
+def populate_dictionary(name, donation):
+    """This function creates a dictionary of donors with names and giving amounts as key-value pairs. """
+    for i in range(len(donor_data)):
+        if donor_data[i]['name'] == name:
+            donor_data[i]['donations'].append(donation)
+        else:
+            donor_data.append({'name': name, 'donations': [donation]})
+            break
+    print (donor_data)
+
+populate_dictionary('Paul Miller', 10000)
+
+# def prompt_user():
+#     returns two options to user:
+#     send_thank_you()
+#     create_report()
+#     if quit:
+#         exit()
+
+# def send_thank_you():
+#     """This function will send a personalized thank you not to the user."""
+#     prompt for full name input
+#     if list is input:
+#         show list of names
+#     elif name:
+#         use it
+#     else:
+#         add name to dictionary and use it
+#     select_amount()
+
+# def select_amount():
+#     """This function recieves the donation amount of a selected individual"""
+#     allow user to input donation amount
+#     if number:
+#         add amount to donation history
+#         create_thank_you()
+#     else:
+#         prompt select_amount() again. 
+
+# def create_thank_you():
+#     """his function creates a personalized thank you note to a donor with the name and amount given included."""
+#     print a thank you note to the terminal in string notation. 
+#     send_thank_you()
+
+# def create_report():
+#     """This function prints a report of the giving history of all donors with their names."""
+#     if quit:
+#         create_data()
+#     print report including the donor's name, giving total, giving frequency, and average gift.
+#     create_data()
