ca72098fafafad2962ee4f9cd258af6cadd74ccb
Mark Reynoso <mreynoso@spu.edu>
2017-10-23 14:07:20 -0700

    added tests for create report function and send thank you

--- src/test_mail_room_madness.py
+++ src/test_mail_room_madness.py
@@ -13,7 +13,7 @@ DATA = [
 
 @pytest.mark.parametrize('a, b', DATA)
 def test_populate_dictionary(a, b):
-    """ensures that populate_dictionary() returns added value to dict"""
+    """Ensures that populate_dictionary() returns added value to dict"""
     from mail_room_madness import populate_dictionary
     for i in range(4):
         output = populate_dictionary(a, b)
@@ -26,3 +26,66 @@ def test_populate_dictionary_type(a, b):
     for i in range(4):
         output = populate_dictionary(a, b)
         assert type(output) == dict
+
+
+THANKS = [
+    ('Chill Dude', 30, '\nDear Chill Dude,\n Thank you for your generous donation of $30. Your support is making a difference in our community.\nSincerely,\nMark and Kavdi\nDirectors of Good\n'),
+    ('Bill Goat', 300, '\nDear Bill Goat,\n Thank you for your generous donation of $300. Your support is making a difference in our community.\nSincerely,\nMark and Kavdi\nDirectors of Good\n'),
+    ('Ed Ucation', 9000, '\nDear Ed Ucation,\n Thank you for your generous donation of $9000. Your support is making a difference in our community.\nSincerely,\nMark and Kavdi\nDirectors of Good\n'),
+    ('Friend Ly', 1, '\nDear Friend Ly,\n Thank you for your generous donation of $1. Your support is making a difference in our community.\nSincerely,\nMark and Kavdi\nDirectors of Good\n')
+]
+
+
+@pytest.mark.parametrize('a, b, result', THANKS)
+def test_send_thank_you(a, b, result):
+    """tests a non-numericalal value in send_thank_you()"""
+    from mail_room_madness import send_thank_you
+    assert send_thank_you(a, b) == result
+
+
+NAMES = [
+    ('Phil Collins'),
+    ('Sven Sunguaard')
+]
+
+
+@pytest.mark.parametrize('result', NAMES)
+def test_names_in_create_report(result):
+    """Tests if database names are included in create_report()."""
+    from mail_room_madness import create_report
+    out = create_report()
+    assert out.find(result)
+
+
+AMT = [
+    ('25'),
+    ('45'),
+    ('76'),
+    ('100'),
+    ('50'),
+    ('1000'),
+    ('76'),
+    ('1400')
+]
+
+
+@pytest.mark.parametrize('result', AMT)
+def test_donation_amounts_in_create_report(result):
+    """Tests if each donation is included in create_report()."""
+    from mail_room_madness import create_report
+    out = create_report()
+    assert out.find(result)
+
+
+AVG = [
+    ('246'),
+    ('2526')
+]
+
+
+@pytest.mark.parametrize('result', AVG)
+def test_avg_donation_in_create_report(result):
+    """Tests if the average donation amount is included in create_report()."""
+    from mail_room_madness import create_report
+    out = create_report()
+    assert out.find(result)
